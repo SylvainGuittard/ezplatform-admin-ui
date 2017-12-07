1f87bd87043325cebc588d8ed99f09212b12a51f
Piotr Nalepa <piotr.nalepa@ez.no>
2017-12-07 14:11:21 +0100

    fixup frontend

--- src/bundle/Resources/public/js/scripts/fieldType/ezobjectrelationlist.js
+++ src/bundle/Resources/public/js/scripts/fieldType/ezobjectrelationlist.js
@@ -3,6 +3,7 @@
     const SELECTOR_INPUT = '.ez-data-source__input';
     const SELECTOR_FIELD_INPUT = `${SELECTOR_FIELD} ${SELECTOR_INPUT}`;
     const SELECTOR_LABEL_WRAPPER = '.ez-field-edit__label-wrapper';
+    const SELECTOR_BTN_ADD = '.ez-relations__table-action--create';
 
     class EzObjectRelationListValidator extends global.eZ.BaseFieldValidator {
         /**
@@ -47,22 +48,26 @@
         const relationsContainer = fieldContainer.querySelector('.ez-relations__list');
         const relationsWrapper = fieldContainer.querySelector('.ez-relations__wrapper');
         const relationsCTA = fieldContainer.querySelector('.ez-relations__cta');
+        const addBtn = fieldContainer.querySelector(SELECTOR_BTN_ADD)
+        const selectedItemsLimit = 6;
         const closeUDW = () => udwContainer.innerHTML = '';
-        const updateInputValue = (items) => sourceInput.value = items.map(item => item.id).join();
+        const updateInputValue = (items) => sourceInput.value = items.join();
         const onConfirm = (items) => {
             items = excludeDuplicatedItems(items);
-            selectedItems = [...selectedItems, ...items];
+
+            renderRows(items);
+
+            selectedItems = [...selectedItems, ...items.map(item => item.id)];
 
             updateInputValue(selectedItems);
-            renderRows(items);
             closeUDW();
-
-            relationsWrapper.removeAttribute('hidden');
-            relationsCTA.setAttribute('hidden', true);
+            updateFieldState();
+            updateAddBtnState();
         };
         const onCancel = () => closeUDW();
         const canSelectContent = (item, callback) => {
-            const canSelect = !selectedItems.find(selectedItem => selectedItem.id === item.id);
+            const canSelect = selectedItems.length < selectedItemsLimit &&
+                !selectedItems.find(id => id === item.id);
 
             callback(canSelect);
         };
@@ -75,7 +80,7 @@
                 confirmLabel: 'Confirm selection',
                 title: 'Select content',
                 multiple: true,
-                selectedItemsLimit: 3,
+                selectedItemsLimit,
                 restInfo: { token, siteaccess },
                 canSelectContent
             }), udwContainer);
@@ -92,18 +97,49 @@
                     <td>${item.ContentInfo.Content.Name}</td>
                     <td>${item.ContentInfo.Content.ContentType._href}</td>
                     <td>${(new Date(item.ContentInfo.Content.publishedDate)).toLocaleString()}</td>
-                    <td><input class="ez-relations__order-input" type="number" value="${index + 1}" /></td>
+                    <td><input class="ez-relations__order-input" type="number" value="${selectedItems.length + index + 1}" /></td>
                 </tr>
             `;
         };
         const renderRows = (items) => items.forEach((...args) => relationsContainer.insertAdjacentHTML('beforeend', renderRow(...args)));
+        const updateFieldState = () => {
+            const wrapperMethod = selectedItems.length ? 'removeAttribute' : 'setAttribute';
+            const ctaMethod = selectedItems.length ? 'setAttribute' : 'removeAttribute';
+
+            relationsWrapper[wrapperMethod]('hidden', true);
+            relationsCTA[ctaMethod]('hidden', true);
+        };
+        const updateAddBtnState = () => {
+            const methodName = selectedItems.length < selectedItemsLimit ? 'removeAttribute' : 'setAttribute';
+
+            addBtn[methodName]('disabled', true);
+        };
+        const removeItem = (event) => {
+            event.preventDefault();
+
+            const removedItems = [];
+
+            [...relationsContainer.querySelectorAll('input:checked')].forEach(input => {
+                removedItems.push(parseInt(input.value, 10));
+
+                input.closest('tr').remove();
+            });
+
+            selectedItems = selectedItems.filter(item => !removedItems.includes(item));
+
+            updateInputValue(selectedItems);
+            updateFieldState();
+            updateAddBtnState();
+        };
         let selectedItems = [];
         let selectedItemsMap = {};
 
         [
-            ...fieldContainer.querySelectorAll('.ez-relations__table-action--create'),
+            ...fieldContainer.querySelectorAll(SELECTOR_BTN_ADD),
             ...fieldContainer.querySelectorAll('.ez-relations__cta-btn')
         ].forEach(btn => btn.addEventListener('click', openUDW, false));
+
+        fieldContainer.querySelector('.ez-relations__table-action--remove').addEventListener('click', removeItem, false);
 
         validator.init();
 
